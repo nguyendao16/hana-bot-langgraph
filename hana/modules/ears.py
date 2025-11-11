63aa02b7639653701d3432d4ab938189d694406c
Nguyen Dao <daokhoinguyen.go@gmail.com>
2025-11-11 20:25:34 +0700

    Hana Base Ears

---
+++ hana/modules/ears.py
@@ -0,0 +1,20 @@
+from RealtimeSTT import AudioToTextRecorder
+from langchain_core.messages import HumanMessage
+class Ears:
+    def __init__(self, powerby: str = None):
+        self.recorder = AudioToTextRecorder(model=powerby)
+    def __call__(self, state: dict):
+        print("Hana is listening...")
+        message = self.listening()
+        state["messages"] = [HumanMessage(content=message)]
+        return state
+
+    def listening(self):
+        input("Press Enter to start recording...")
+        print("Press Enter again to stop recording... ")
+        self.recorder.start()
+        input()
+        
+        self.recorder.stop()
+        text = self.recorder.text()
+        return text.strip()
